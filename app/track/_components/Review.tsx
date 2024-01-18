f6f41d7ed4a7d621f680fe815579a8c0c5ac79f5
Austinobravo <Austinobravo@gmail.com>
2024-01-18 19:47:22 +0100

    modified more button

--- app/track/_components/Review.tsx
+++ app/track/_components/Review.tsx
@@ -10,7 +10,7 @@ const Review = () => {
         <div className='space-y-7  w-full px-3'>
             <h2 className='font-bold text-xl'>ABC123</h2>
             <p className='text-black/50 text-xs'>We found this shipment for your tracking code. </p>
-            <div className='flex justify-between flex-wrap md:flex-nowrap gap-x-7 md:py-3 pt-2 px-3 border-y-2 pb-4 border-black/50 w-full'>
+            <div className='flex justify-between flex-wrap md:flex-nowrap gap-x-7 md:py-3 pt-2 px-3 border-y-2 pb-4 border-black/50 '>
                 <Check size={30} color='green'/>
                 <div className='flex flex-col space-y-5 pb-2  flex-wrap md:flex-nowrap w-full'>
                     <div className='flex items-center '>
@@ -23,9 +23,10 @@ const Review = () => {
                         <ArrowRight size={10}/>
                         <p>Auckland, New Zealand</p>
                     </div>
-
                 </div>
-                <button className='border-red-500 border-2 py-1 w-fit  text-red-500 px-4 rounded-md'>More</button>
+                <div className='flex justify-center items-center w-fit'>
+                    <button className='border-red-500 border-2 py-1 w-fit  text-red-500 hover:bg-red-500 hover:text-white hover:border-0 px-4 rounded-md'>More</button>
+                </div>
             </div>
         </div>
     </div>
